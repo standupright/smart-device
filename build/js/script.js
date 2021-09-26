833c91b05186ed3e3294de1c65720fd6dc83c3eb
standupright <standupright@mail.ru>
2021-09-27 01:24:28 +0500

    add nojs accordeon

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,9 @@
 const accordeonButtons = document.querySelectorAll('#accordeon-button');
-const activeAccordeonClass = 'accordeon--active';
+const ACTIVE_ACCORDEON_CLASS = 'accordeon__button--active';
+const NOJS_ACCORDEON_CLASS = 'accordeon__button--nojs';
+const NOJS_WRAPPER_CLASS = 'accordeon__wrapper--nojs';
 
+// accordeon
 const resetMaxHeight = (element) => {
   element.style.maxHeight = null;
 };
@@ -8,19 +11,19 @@ const resetMaxHeight = (element) => {
 const closeAccordeons = (currentAccordeon, allAccordeons) => {
   for (let i = 0; i < allAccordeons.length; i++) {
     if (
-      allAccordeons[i].classList.contains(activeAccordeonClass)
+      allAccordeons[i].classList.contains(ACTIVE_ACCORDEON_CLASS)
       &&
       currentAccordeon !== allAccordeons[i]
     ) {
       resetMaxHeight(allAccordeons[i].nextElementSibling);
-      allAccordeons[i].classList.remove(activeAccordeonClass);
+      allAccordeons[i].classList.remove(ACTIVE_ACCORDEON_CLASS);
     }
   }
 };
 
 const addAccordeonButtonsHandler = function (button) {
   button.addEventListener('click', function () {
-    this.classList.toggle(activeAccordeonClass);
+    this.classList.toggle(ACTIVE_ACCORDEON_CLASS);
     const wrapper = this.nextElementSibling;
     if (wrapper.style.maxHeight) {
       resetMaxHeight(wrapper);
@@ -32,5 +35,9 @@ const addAccordeonButtonsHandler = function (button) {
 };
 
 for (let i = 0; i < accordeonButtons.length; i++) {
-  addAccordeonButtonsHandler(accordeonButtons[i]);
+  if (accordeonButtons[i]) {
+    addAccordeonButtonsHandler(accordeonButtons[i]);
+    accordeonButtons[i].classList.remove(NOJS_ACCORDEON_CLASS);
+    accordeonButtons[i].nextElementSibling.classList.remove(NOJS_WRAPPER_CLASS);
+  }
 }
