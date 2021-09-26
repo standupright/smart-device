439a8b6416ff46723e9a082afef81831c8cc5365
standupright <standupright@mail.ru>
2021-09-27 01:05:02 +0500

    finish accrodeon and mobile/tablet view on footer Block

--- build/js/script.js
+++ build/js/script.js
@@ -1,86 +1,36 @@
-const NAV_CLOSED = 'nav--closed';
-const NAV_NOJS = 'nav--nojs';
-const NAV_OPENED = 'nav--opened';
-const OVERFLOW = 'overflow-hidden';
-const nav = document.querySelector('.nav');
-const navToggle = document.querySelector('.nav__toggle');
-const body = document.querySelector('#body');
-const form = document.querySelector('.form-field');
-const nameForm = document.querySelector('#name');
-const telForm = document.querySelector('#tel');
+const accordeonButtons = document.querySelectorAll('#accordeon-button');
+const activeAccordeonClass = 'accordeon--active';
 
+const resetMaxHeight = (element) => {
+  element.style.maxHeight = null;
+};
 
-// Плавный скролл
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (const smoothLink of smoothLinks) {
-  if (smoothLink) {
-    smoothLink.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      const id = smoothLink.getAttribute('href');
-
-      const placeScroll = document.querySelector(id);
-
-      if (placeScroll) {
-        placeScroll.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start',
-        });
-      }
-    });
+const closeAccordeons = (currentAccordeon, allAccordeons) => {
+  for (let i = 0; i < allAccordeons.length; i++) {
+    if (
+      allAccordeons[i].classList.contains(activeAccordeonClass)
+      &&
+      currentAccordeon !== allAccordeons[i]
+    ) {
+      resetMaxHeight(allAccordeons[i].nextElementSibling);
+      allAccordeons[i].classList.remove(activeAccordeonClass);
+    }
   }
-}
+};
 
-// Валидация формы
-if (nameForm) {
-  nameForm.addEventListener('invalid', () => {
-    if (nameForm.validity.valueMissing) {
-      nameForm.setCustomValidity('Обязательное поле');
+const addAccordeonButtonsHandler = function (button) {
+  button.addEventListener('click', function () {
+    this.classList.toggle(activeAccordeonClass);
+    const wrapper = this.nextElementSibling;
+    if (wrapper.style.maxHeight) {
+      resetMaxHeight(wrapper);
     } else {
-      nameForm.setCustomValidity('');
+      wrapper.style.maxHeight = `${wrapper.scrollHeight}px`;
+      closeAccordeons(this,accordeonButtons);
     }
   });
-}
+};
 
-if (telForm) {
-  telForm.addEventListener('invalid', () => {
-    if (telForm.validity.valueMissing || telForm.validity.patternMismatch) {
-      telForm.setCustomValidity('Неверный номер телефона');
-    } else {
-      telForm.setCustomValidity('');
-    }
-  });
-}
-
-let isStorageSupport = true;
-let storageName = '';
-let storageTel = '';
-
-try {
-  storageName = localStorage.getItem('name');
-  storageTel = localStorage.getItem('tel');
-} catch (err) {
-  isStorageSupport = false;
-}
-
-if (storageName && nameForm) {
-  nameForm.value = storageName;
-}
-
-if (storageTel && telForm) {
-  telForm.value = storageTel;
-}
-
-// Событие отправки формы
-if (form) {
-  form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-
-    if (isStorageSupport) {
-      localStorage.setItem('name', nameForm.value);
-      localStorage.setItem('tel', telForm.value);
-    }
-
-    nameForm.value = '';
-    telForm.value = '';
-  });
+for (let i = 0; i < accordeonButtons.length; i++) {
+  addAccordeonButtonsHandler(accordeonButtons[i]);
 }
